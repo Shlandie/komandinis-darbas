ca5725fc4e5265904e158abaf73584f752ac1c2b
ugnead <u.adomaityte98@gmail.com>
2023-03-29 17:02:23 +0300

    add income page

--- src/components/Pajamu-sekc/Pajamu-sekc.js
+++ src/components/Pajamu-sekc/Pajamu-sekc.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import './Pajamu-sekc.css';
+import React from "react";
+import "./Pajamu-sekc.css";
+import { Link } from "react-router-dom";
 
 function PajamuSekc() {
-  return (
-    <div class="col rounded-3 p-4 Container">
-        <h5 class="d-flex justify-content-between">
-            <div>Pajamos</div>
-            <div>900eur</div>
-        </h5>
-        <div class="d-flex justify-content-between mt-1 mb-1">
-            <div>Pavadinimas<small class="fw-light">data</small></div>
-            <div class="text-success">suma</div>
+    return (
+        <div class="col-4 rounded-3 p-4 Container">
+            <h5 class="d-flex justify-content-between">
+                <div>Pajamos</div>
+                <div>900eur</div>
+            </h5>
+            <div class="d-flex justify-content-between mt-1 mb-1">
+                <div>
+                    Pavadinimas<small class="fw-light">data</small>
+                </div>
+                <div class="text-success">suma</div>
+            </div>
+            <div class="d-flex justify-content-between mt-1 mb-1">
+                <div>
+                    Pavadinimas<small class="fw-light">data</small>
+                </div>
+                <div class="text-success">suma</div>
+            </div>
+            <button type="button" class="btn btn-outline-primary">
+                <Link className="nav-link" to="/pajamu-isplestine">
+                Išskleisti
+                </Link>
+            </button>
         </div>
-        <div class="d-flex justify-content-between mt-1 mb-1">
-            <div>Pavadinimas<small class="fw-light">data</small></div>
-            <div class="text-success">suma</div>
-        </div>
-        <button type="button" class="btn btn-outline-primary">Išskleisti</button>
-    </div>
-  )
-};
+    );
+}
 
 export default PajamuSekc;
