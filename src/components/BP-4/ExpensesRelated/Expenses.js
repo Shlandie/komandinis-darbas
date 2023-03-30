40fb631c5de6eb07f978bcd5929a81359678f82d
Karolis <karolissaganas@yahoo.co.uk>
2023-03-30 19:49:04 +0300

    Removed border, fixed font sizing

--- src/components/BP-4/ExpensesRelated/Expenses.js
+++ src/components/BP-4/ExpensesRelated/Expenses.js
@@ -8,7 +8,7 @@ export default function Išlaidos() {
         <div className='scrollbar-trackDot'></div>
 
         <div className='expense-box'>
-            <h3 className='d-flex justify-content-between'><span>Transportas</span> <span className='pe-2'>150 eur</span></h3>
+            <h3 className='d-flex justify-content-between'><span className='fs-28px'>Transportas</span> <span className='pe-2'>150 eur</span></h3>
             
             <div className='bar'>
                 <div className='blueBar barFontSize d-flex align-items-center p-3'>85%</div>
@@ -16,7 +16,7 @@ export default function Išlaidos() {
         </div>
       
         <div className='expense-box'>
-            <h3 className='d-flex justify-content-between'><span>Maistas</span> <span className='pe-2'>200 eur</span></h3>
+            <h3 className='d-flex justify-content-between'><span className='fs-28px'>Maistas</span> <span className='pe-2'>200 eur</span></h3>
             
             <div className='bar'>
                 <div className='greenBar barFontSize d-flex align-items-center p-3'>50%</div>
@@ -24,7 +24,7 @@ export default function Išlaidos() {
         </div>
 
         <div className='expense-box'>
-            <h3 className='d-flex justify-content-between'><span>Sportas</span> <span className='pe-2'>100 eur</span></h3>
+            <h3 className='d-flex justify-content-between'><span className='fs-28px'>Sportas</span> <span className='pe-2'>100 eur</span></h3>
             
             <div className='bar'>
                 <div className='redBar barFontSize d-flex align-items-center p-3'>20%</div>
