23b5f01b7464919c282c07b84a0babaf9a9e4acc
Karolis <karolissaganas@yahoo.co.uk>
2023-04-03 10:39:22 +0300

    More changes with BP4 and BP9 folders, specifically with BudgetBP4 and EndResult folders. (Working on routes)

--- src/components/BP-9/EndResult/EndResult.js
+++ src/components/BP-9/EndResult/EndResult.js
@@ -4,10 +4,11 @@ import SearchBP9 from '../Parts/Search2/SearchBP9';
 import NewPostBP3 from '../Parts/NewPost3/NewPostBP3';
 import ListBP9 from '../Parts/theList4/ListBP9';
 import './EndResultBP9.css';
+// import { useRef } from 'react';
 
 export default function EndResultBP9() {
   return (
-    <div className='EndResultBP9-container'>
+    <div className='EndResultBP9Container' /* ref={EndResultBP9Container} */>
       <div className='EndResultBP9-flex'>
         <div><BudgetBP9 /></div>
         <div><SearchBP9 /></div>
