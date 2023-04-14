d66114aa02296df816e364d19480bab1247737b4
Karolis <karolissaganas@yahoo.co.uk>
2023-04-14 22:01:16 +0300

    Working on Budget's design

--- src/components/Biudzeto-ispl/Parts/NewPost3/NewPostBP3.js
+++ src/components/Biudzeto-ispl/Parts/NewPost3/NewPostBP3.js
@@ -6,7 +6,7 @@ import {RxTriangleDown} from 'react-icons/rx';
 
 export default function NewPostBP3() {
   return (
-    <div className='BP9container3 fontClr fs-20'>
+    <div className='BP9container3 fontClr fs-20 BP9-child3'>
         <h3 className='BP9-3-heading fs-25'>Naujas įrašas</h3>
             <div className='BP9clrGray'>
                 <div className='BP9selectContainer'><span className=''>Kategorija</span><div className='icon-size'><RxTriangleDown /></div></div>
