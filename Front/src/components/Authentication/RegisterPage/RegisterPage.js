d5ff3da3ef40fb16065dbd7d9ed3f3a20fe1162a
Karolis <karolissaganas@yahoo.co.uk>
2023-05-04 20:03:32 +0300

    Made and worked on the layout of the signup form

--- Front/src/components/Authentication/RegisterPage/RegisterPage.js
+++ Front/src/components/Authentication/RegisterPage/RegisterPage.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div className='loginPage-container fs-'>
+        <form className='loginForm-container p-5'>
+
+            <h1 className='pb-5'>Login Form/Signup Form</h1>
+            
+            <button type='button' className='logInButton width50'>Login</button>
+            <button type='button' className='signUpButton width50 mb-4'>Signup</button>
+            
+            <br></br>
+
+            <label for="email"></label>
+            <input type="email" id="email" placeholder='Email Address' className='FormButton ps-4 mb-3'></input>
+            
+            <br></br>
+            
+            <label for="password"></label>
+            <input type="password" id="password" placeholder='Password' className='FormButton ps-4 mb-3'></input>
+            
+            <label for="password2"></label>
+            <input type="password" id="password2" placeholder='Confirm Password' className='FormButton ps-4 mb-3'></input>
+            <br></br>
+
+            <button type='button' className='FormButton'>Signup</button>
+            <p className='signUpNowLink mt-5'>Not a member? <a href="#">Signup now!</a> </p>
+        </form>
+    </div>
   )
 }
 
