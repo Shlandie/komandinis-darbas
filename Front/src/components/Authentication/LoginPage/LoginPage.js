3a98372c1f60f543236dfaabf814313b7dceaf14
Karolis <karolissaganas@yahoo.co.uk>
2023-05-07 08:40:31 +0300

    Changed the first two buttons sizes, shapes. Added on all the main buttons a hover effect(on and off). Removed and colored the border that needed to be removed and colored (followed the example in Figma)

--- Front/src/components/Authentication/LoginPage/LoginPage.js
+++ Front/src/components/Authentication/LoginPage/LoginPage.js
@@ -6,23 +6,20 @@ function LoginPage() {
         <form className='AuthenticationForm-container p-5'>
 
             <h1 className='pb-5'>Login Form/Signup Form</h1>
+            <div className='d-flex justify-content-between mb-4'>
+                <button type='button' className='logInButton width50'>Login</button>
+                <button type='button' className='signUpButton width50'>Signup</button>
+            </div>
             
-            <button type='button' className='logInButton width50'>Login</button>
-            <button type='button' className='signUpButton width50 mb-3'>Signup</button>
-            
-            <br></br>
-
             <label for="email"></label>
             <input type="email" id="email" placeholder='Email Address' className='FormButton ps-4 mb-3'></input>
             
-            <br></br>
             
             <label for="password"></label>
             <input type="password" id="password" placeholder='Password' className='FormButton ps-4'></input>
             <p className='mb-4'><a href="#">Forgot password?</a></p>
-            <br></br>
 
-            <button type='button' className='FormButton'>Login</button>
+            <button type='button' className='FormButton FormButtonColor'>Login</button>
             <p className='signUpNowLink mt-5'>Not a member? <a href="#">Signup now!</a> </p>
         </form>
     </div>
--- Front/src/components/Authentication/RegisterPage/RegisterPage.js
+++ Front/src/components/Authentication/RegisterPage/RegisterPage.js
@@ -6,25 +6,21 @@ function RegisterPage() {
         <form className='AuthenticationForm-container p-5'>
 
             <h1 className='pb-5'>Login Form/Signup Form</h1>
+            <div className='d-flex justify-content-between mb-4'>
+                <button type='button' className='logInButton width50'>Login</button>
+                <button type='button' className='signUpButton width50'>Signup</button>
+            </div>
             
-            <button type='button' className='logInButton width50'>Login</button>
-            <button type='button' className='signUpButton width50 mb-3'>Signup</button>
-            
-            <br></br>
-
             <label for="email"></label>
             <input type="email" id="email" placeholder='Email Address' className='FormButton ps-4 mb-3'></input>
-            
-            <br></br>
             
             <label for="password"></label>
             <input type="password" id="password" placeholder='Password' className='FormButton ps-4 mb-3'></input>
             
             <label for="password2"></label>
-            <input type="password" id="password2" placeholder='Confirm Password' className='FormButton ps-4 mb-3'></input>
-            <br></br>
+            <input type="password" id="password2" placeholder='Confirm Password' className='FormButton ps-4 mb-4'></input>
 
-            <button type='button' className='FormButton'>Signup</button>
+            <button type='button' className='FormButton FormButtonColor'>Signup</button>
             <p className='signUpNowLink mt-5'>Not a member? <a href="#">Signup now!</a> </p>
         </form>
     </div>
