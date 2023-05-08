844264a71a28186124db0d4c8715a154fd4f754a
Karolis <karolissaganas@yahoo.co.uk>
2023-05-08 18:34:37 +0300

    experimental logo, could be changed in the future

--- Front/src/components/Navigation/Navigation.js
+++ Front/src/components/Navigation/Navigation.js
@@ -7,7 +7,10 @@ import { Link } from "react-router-dom";
 function Navigation() {
     return (
         <nav className="navbar Nav-container BP38-child0">
-            <h2 className="navbar-brand mt-2 F-size-25 Roboto-condensed">LOGO</h2>
+            <h2 className="navbar-brand mt-2 F-size-25 Roboto-condensed">
+                <div className="logo">
+                </div><span className="logo-title">campl</span>
+            </h2>
             <div className="d-flex justify-content-end nameLogoutContainer">
                 <div class="Roboto-condensed F-size-25 Nav-greeting">
                     Sveiki, Vardas!
