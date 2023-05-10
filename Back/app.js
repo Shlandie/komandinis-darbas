2b3792e34bd2db1719f631243a991f8d831134c8
Simona <simmona92@gmail.com>
2023-05-10 14:31:20 +0300

    dalis darbo padaryta

--- Back/app.js
+++ Back/app.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const { User, Expense, Earning } = require("./models/models");
 
 const mainRouter = require("./routes/mainRoutes");
-// const incomesRouter = require("./routes/incomeRoutes");
+const incomesRouter = require("./routes/incomeRoutes");
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Content-Type, Authorization"
-//     );
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 
 app.get("/expenses/:month", async (req, res) => {
@@ -36,7 +34,8 @@ app.get("/expenses/:month", async (req, res) => {
     });
 })
 // app.use("/", mainRouter);
+app.use(cors());
 
-// app.use("/api/v1/incomes", incomesRouter);
+app.use("/api/v1/incomes", incomesRouter);
 
 module.exports = app;
