194a6a954482f5a6a588bf7d25f247f0df4db55e
Karolis <karolissaganas@yahoo.co.uk>
2023-05-16 12:13:15 +0300

    fixed buttons color

--- Front/src/components/Biudzeto-ispl/Parts/buttons1/BP9buttons.js
+++ Front/src/components/Biudzeto-ispl/Parts/buttons1/BP9buttons.js
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom';
 export default function BP9buttons() {
   return (
     <div className="row d-flex g-0  IncomeNav gridChild-1 Roboto-condensed F-size-20">
-                    <Link to='/pajamu-isplestine' className='BP9-btn colorAnchored BP9-selectedbtn'>
+                    <Link to='/pajamu-isplestine' className='BP9-btn colorAnchored'>
                         <div>Pajamos</div> </Link>
 
                     <Link to='/islaidu-isplestine' className='BP9-btn colorAnchored'>
                         <div>Išlaidos</div> </Link>
 
-                    <Link to='/biudzeto-isplestine' className='BP9-btn colorAnchored'>
+                    <Link to='/biudzeto-isplestine' className='BP9-btn colorAnchored BP9-selectedbtn'>
                         <div>Biudžetas</div> </Link>
                 </div>
   )
--- Front/src/components/Islaidu-ispl/Islaidu-ispl/Islaidu-ispl.js
+++ Front/src/components/Islaidu-ispl/Islaidu-ispl/Islaidu-ispl.js
@@ -213,10 +213,10 @@ function IslaiduIspl() {
             <div className="g-0 Income-wrapper gridParent-0">
                 <Navigation />
                 <div className="row d-flex g-0  IncomeNav gridChild-1 Roboto-condensed F-size-20">
-                    <Link to='/pajamu-isplestine' className='BP9-btn colorAnchored BP9-selectedbtn'>
+                    <Link to='/pajamu-isplestine' className='BP9-btn colorAnchored'>
                         <div>Pajamos</div> </Link>
 
-                    <Link to='/islaidu-isplestine' className='BP9-btn colorAnchored'>
+                    <Link to='/islaidu-isplestine' className='BP9-btn colorAnchored BP9-selectedbtn'>
                         <div>Išlaidos</div> </Link>
 
                     <Link to='/biudzeto-isplestine' className='BP9-btn colorAnchored'>
