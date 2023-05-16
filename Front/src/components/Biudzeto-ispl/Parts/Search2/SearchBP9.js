a76c2cce052a8ab29f1e5a67053783abc67a303e
Karolis <karolissaganas@yahoo.co.uk>
2023-05-16 11:35:20 +0300

    Solved the issues related to bug-79

--- Front/src/components/Biudzeto-ispl/Parts/Search2/SearchBP9.js
+++ Front/src/components/Biudzeto-ispl/Parts/Search2/SearchBP9.js
@@ -10,7 +10,10 @@ export default function SearchBP9() {
     <div className='BP9container2 fontClr fs-20 BP9-child2'>
         <h3 className='BP9-2-heading fs-25'>Paieška</h3>
 
-        <select className="BP9selectContainer BP9clrGray" aria-label="Default select example" id="programSelect" name="programSelect" required>
+        <select className="form-select IncomeNewEntry-input F-size-19 Roboto-condensed"
+        aria-label="Default select example"
+        id="programSelect"
+        name="programSelect">
           <option defaultValue disabled>Kategorija</option>
           <option>Transportas</option>
           <option>Maistas ir gėrimai</option>
