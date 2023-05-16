964f8d8b67598d7405090e54dd5f7000d1862cfb
Karolis <karolissaganas@yahoo.co.uk>
2023-05-16 11:51:48 +0300

    The search container is now aligned and matching on all three pages

--- Front/src/components/Biudzeto-ispl/Parts/Search2/SearchBP9.js
+++ Front/src/components/Biudzeto-ispl/Parts/Search2/SearchBP9.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './SearchBP9.css';
 import '../../BP9global.css';
-import {RxTriangleDown} from 'react-icons/rx';
+import { RxTriangleDown } from 'react-icons/rx';
 
 
 
 export default function SearchBP9() {
   return (
     <div className='BP9container2 fontClr fs-20 BP9-child2'>
-        <h3 className='BP9-2-heading fs-25'>Paieška</h3>
+      <h4 className="Roboto-condensed F-size-25 IncomeSearch-title">Paieška</h4>
 
-        <select className="form-select IncomeNewEntry-input F-size-19 Roboto-condensed"
+      <select className="form-select IncomeNewEntry-input F-size-19 Roboto-condensed"
         aria-label="Default select example"
         id="programSelect"
         name="programSelect">
-          <option defaultValue disabled>Kategorija</option>
-          <option>Transportas</option>
-          <option>Maistas ir gėrimai</option>
-          <option>Pramogos</option>
-          <option>Mokesčiai</option>
-          <option>Paslaugos</option>
-          <option>Pirkiniai ir daiktai</option>
-          <option>Kitos išlaidos</option>
-          <div className='icon-size iconGray'><RxTriangleDown /></div>  
-        </select>
+        <option defaultValue disabled>Kategorija</option>
+        <option>Transportas</option>
+        <option>Maistas ir gėrimai</option>
+        <option>Pramogos</option>
+        <option>Mokesčiai</option>
+        <option>Paslaugos</option>
+        <option>Pirkiniai ir daiktai</option>
+        <option>Kitos išlaidos</option>
+        <div className='icon-size iconGray'><RxTriangleDown /></div>
+      </select>
 
-        <div className='fontClr BP9button'>Ieškoti</div>
+      <div className='fontClr BP9button'>Ieškoti</div>
     </div>
   )
 }
