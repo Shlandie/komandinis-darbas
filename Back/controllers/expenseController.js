80611aceeecec29e7d6811c8413da13bdba853ea
ugnead <u.adomaityte98@gmail.com>
2023-05-18 08:48:33 +0300

    connect back end to expense page

--- Back/controllers/expenseController.js
+++ Back/controllers/expenseController.js
@@ -18,21 +18,59 @@ exports.getAllExpenses = async (req, res) => {
     }
 };
 
-exports.delete = async (req, res) => {
+exports.deleteExpense = async (req, res) => {
     try {
         const { id } = req.params;
-        const deleteExpense = await Expense.findByIdAndDelete(id);
+        const expense = await Expense.findByIdAndDelete(id);
 
-        if (!deleteExpense) {
+        if (!expense) {
             return res.status(404).json({ msg: `No expense with id: ${id}` });
         } else {
             res.status(200).json({
                 status: "success",
                 message: `Expense with id: ${id} deleted successfully.`,
-                expense: deleteExpense,
+                expense: expense,
             });
         }
     } catch (error) {
         res.status(500).json({ error: error.message });
+    }
+};
+
+exports.createNewExpense = async (req, res) => {
+    try {
+        const newExpense = await Expense.create(req.body);
+        res.status(201).json({
+            status: "success",
+            data: {
+                expense: newExpense,
+            },
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "fail",
+            message: err.message,
+        });
+    }
+};
+
+exports.updateExpense = async (req, res) => {
+    try {
+        const upatedExpense = await Expense.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json({
+            status: "success",
+            data: {
+                expense: upatedExpense,
+            },
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "fail",
+            message: err.message,
+        });
     }
 };
